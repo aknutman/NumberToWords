b039dd92304d332314a8443b75c996ee7da34b18
Eldest Arif Pasirula <eldest.pasirula@hotmail.com>
2022-08-17 07:36:52 +0700

    remove unused using

--- NumberToWords/SolutionRunner/FirstSR.cs
+++ NumberToWords/SolutionRunner/FirstSR.cs
@@ -1,9 +1,4 @@
 ﻿using NumberToWords.Solutions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NumberToWords.SolutionRunner
 {
